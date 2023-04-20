aabd83bae44a549d895e85b24d381c7a40f7067b
clement <clement.conchezboueytou@edu.ece.fr>
2023-04-20 11:23:50 +0200

    feat: theme

---
+++ app/components/Banner.js
@@ -0,0 +1,18 @@
+const Banner = () => {
+
+
+
+    return (
+        <div id='banner' > 
+            <div id='rectangle' > </div>
+            <div id='rectangle' > </div>
+            <div id='rectangle' > </div>
+            <div id='rectangle' > </div>
+            <div id='rectangle' > </div>
+            <div id='rectangle' > </div>
+        
+         </div>
+    )
+}
+
+export default Banner
---
+++ app/components/Banner_top.js
@@ -0,0 +1,18 @@
+const Banner_top = () => {
+
+
+
+    return (
+        <div id='banner_top' > 
+            <div id='parallelogram' > a </div>
+            <div id='parallelogram' > a</div>
+            <div id='parallelogram' >a </div>
+            <div id='parallelogram' >a </div>
+            <div id='parallelogram' >a </div>
+            <div id='parallelogram' > a</div>
+        
+         </div>
+    )
+}
+
+export default Banner_top
